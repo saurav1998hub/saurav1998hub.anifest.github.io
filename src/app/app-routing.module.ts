de3a7ec60c97b746b594d3dcb14b915da9825f7d
saurav1998hub <58566328+saurav1998hub@users.noreply.github.com>
2024-01-29 02:46:24 +0530

    dev

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { AppLayoutComponent } from './layouts/app-layout/app-layout.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', pathMatch:'full', redirectTo:'main-page'},
+  {
+    path:'main-page',
+    component: AppLayoutComponent,
+    children:[
+      {path:'', pathMatch:'full', component: LandingPageComponent},
+      {path:'contact', pathMatch:'full', component: ContactComponent},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { AppLayoutComponent } from './layouts/app-layout/app-layout.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    NavbarComponent,
+    AppLayoutComponent,
+    ContactComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
