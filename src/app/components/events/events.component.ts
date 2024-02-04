bf30932aa94fd316ed13cc0967b32a6221a68ac1
saurav1998hub <58566328+saurav1998hub@users.noreply.github.com>
2024-02-05 04:03:50 +0530

    deploy

--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -1,62 +1,79 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { NavigationEnd } from '@angular/router';
-import { filter, first } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter, first } from 'rxjs/operators';
+
+interface EventItem {
+  image_url: string;
+  event_url: string;
+  name: string;
+  description: string;
+  status: string;
+}
+
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.scss']
 })
-export class EventsComponent {
-
-  eventsArray: any[]= [
+export class EventsComponent implements OnInit {
+  eventsArray: EventItem[] = [
     {
-      image_url:"../../../assets/images/Image.jpeg",
-      event_url: "https://www.townscript.com/v2/widget/anifest-303310/booking",
-      name:"Anime FanFest Ranchi",
-      descripton:"Jharkhand's First Comic-Con!",
-      status:"Book Now"
+      image_url: '../../../assets/images/Image.jpeg',
+      event_url: 'https://www.townscript.com/v2/widget/anifest-303310/booking',
+      name: 'Anime FanFest Ranchi',
+      description: 'Jharkhand\'s First Comic-Con!',
+      status: 'Book Now'
     },
     {
-      image_url:"./../../assets/images/Image Feb.jpeg",
-      event_url: "https://www.townscript.com/v2/widget/anifest-303310/booking",
-      name: "BGMI TOURNAMENT",
-      descripton:"Coming Soon!",
-      status:'Coming Soon'
+      image_url: '../../../assets/images/Image Feb.jpeg',
+      event_url: 'https://www.townscript.com/v2/widget/anifest-303310/booking',
+      name: 'BGMI TOURNAMENT',
+      description: 'Coming Soon!',
+      status: 'Coming Soon'
     },
     {
-      image_url:"../../../assets/images/Image Creator (7).jpeg",
-      event_url: "https://www.townscript.com/v2/widget/anifest-303310/booking",
-      name:'Dummy Name',
-      descripton:'Dummy Name',
-      status:'Event Ended'
+      image_url: '../../../assets/images/Image.jpg',
+      event_url: 'https://www.townscript.com/v2/widget/anifest-303310/booking',
+      name: 'AOT',
+      description: 'Meet Up Party',
+      status: 'Event Ended'
     },
     {
-      image_url:"../../../assets/images/Image Creator (7).jpeg",
-      event_url: "https://www.townscript.com/v2/widget/anifest-303310/booking",
-      name:'Dummy Name',
-      descripton:'Dummy Name',
-      status:'Event Ended'
+      image_url: '../../../assets/images/Nov 26 2023 Resized Photo.jpg',
+      event_url: 'https://www.townscript.com/v2/widget/anifest-303310/booking',
+      name: 'JJK',
+      description: 'Meet-up Party',
+      status: 'Event Ended'
+    },
+    {
+      image_url: '../../../assets/images/Dec 24 Photo 202 (1).jpg',
+      event_url: 'https://www.townscript.com/v2/widget/anifest-303310/booking',
+      name: 'I Want To Eat Your Pancreas',
+      description: 'Meetup Party',
+      status: 'Event Ended'
     }
   ];
 
   constructor(private router: Router) {}
 
-  redirectToBooking(event: any) {
-    if(event.status == 'Book Now'){
+  ngOnInit() {
+    this.handleInitialNavigation();
+  }
+
+  redirectToBooking(event: EventItem) {
+    if (event.status === 'Book Now') {
       this.router.navigate(['/BookingPage']);
     }
   }
 
-  ngOnInit() {
-    this.router.events
-      .pipe(
-        filter((event) => event instanceof NavigationEnd),
-        first() // Handle only the initial navigation
-      )
-      .subscribe(() => {
-        // Smoothly scroll to top after initial navigation
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-      });
+  private handleInitialNavigation() {
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      first()
+    ).subscribe(() => this.scrollToTopSmoothly());
+  }
+
+  private scrollToTopSmoothly() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,15 +1,50 @@
-import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Component, ElementRef, OnDestroy, ViewChild, ViewEncapsulation } from "@angular/core";
+
+enum SocialMediaUrls{
+  Instagram = 'https://www.instagram.com/anifestranchi/',
+  YouTube = 'https://youtube.com/@LazyWeebsPodcast?si=lkpRbXGDeVEGXt_1',
+  Discord = 'https://discord.gg/K9AfkRb8',
+}
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
 })
-export class NavbarComponent {
+
+export class NavbarComponent{
+
+  hamburgerOpen: any;
   constructor(private router: Router) {}
 
-  redirectToBooking(url: any) {
-    this.router.navigate([url]);
+  hamburgerClicked() {
+    this.hamburgerOpen = !this.hamburgerOpen;
+  }
+
+  routeToUrl(url: string) {
+    const socialMediaUrl = (SocialMediaUrls as any)[url];
+    if (url in SocialMediaUrls) {
+      this.goToSocial(socialMediaUrl);
+    } else {
+      this.router.navigate([url]);
+    }
+  }
+
+  
+
+  goToSocial(url: string | URL | undefined) {
+    const isSecureContext = window.location.protocol === 'https:';
+
+    if (isSecureContext) {
+      window.open(url, '_blank');
+    } else {
+      alert(
+        'Your current context is not secure enough to open Instagram. Please visit ' +
+          url +
+          ' directly in your browser.'
+      );
+    }
   }
 }
