68252d2b9f7e5c9df6f0efaf24e8ebe3d2ab0547
saurav1998hub <58566328+saurav1998hub@users.noreply.github.com>
2024-03-11 03:09:40 +0530

    registration changes

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -2,7 +2,10 @@ import { Router } from '@angular/router';
 import {
   Component,
   ElementRef,
+  HostListener,
+  Input,
   OnDestroy,
+  OnInit,
   ViewChild,
   ViewEncapsulation,
 } from '@angular/core';
@@ -12,19 +15,34 @@ enum SocialMediaUrls {
   YouTube = 'https://youtube.com/@LazyWeebsPodcast?si=lkpRbXGDeVEGXt_1',
   Discord = 'https://discord.gg/K9AfkRb8',
 }
-
+enum Registrations{
+  Volunteer = 'https://docs.google.com/forms/d/e/1FAIpQLSf1YkjCCb48XtgBQUjSyIsrPEZR27_QNqcBJ7Mio8igCvTF6w/viewform?usp=sf_link',
+  Cosplay = 'https://forms.gle/A4eepuJjm1CBQPNE7',
+}
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
   encapsulation: ViewEncapsulation.Emulated,
 })
-export class NavbarComponent {
+export class NavbarComponent{
+  @ViewChild('targetDiv') targetDiv: ElementRef | null = null;
+  showregstration:boolean = false;
 
-  onClickedOutside() {
-    // if(this.hamburgerOpen){
-    //   this.hamburgerOpen = false; 
-    // }
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: Event) {
+    if (this.targetDiv && this.targetDiv.nativeElement) {
+      const clickedInside = this.targetDiv.nativeElement.contains(event.target);
+      // if (!clickedInside) {
+      //   this.showregstration  = false;
+      // }
+    }
+  }
+  
+  ngAfterViewInit() {
+    if (this.targetDiv && this.targetDiv.nativeElement) {
+      // Access targetDiv here
+    }
   }
 
   hamburgerOpen: boolean = false;
@@ -32,14 +50,22 @@ export class NavbarComponent {
 
   hamburgerClicked() {
     this.hamburgerOpen = !this.hamburgerOpen;
+    this.showregstration = false;
   }
 
   routeToUrl(url: string) {
+    this.showregstration = false;
     const socialMediaUrl = (SocialMediaUrls as any)[url];
+    const registration = (Registrations as any)[url];
     if (url in SocialMediaUrls) {
       this.goToSocial(socialMediaUrl);
       this.hamburgerOpen = false;
-    } else {
+    } 
+    else if(url in Registrations){
+      this.goToSocial(registration);
+      this.hamburgerOpen = false;
+    }
+    else {
       this.router.navigate([url]);
       this.hamburgerOpen = false;
     }
@@ -50,6 +76,7 @@ export class NavbarComponent {
   }
   ngOnDestroy(){
     this.hamburgerOpen = false;
+    this.showregstration = false;
   }
 
 }
