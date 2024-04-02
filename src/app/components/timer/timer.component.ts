6d5656e5750b7de0bf8c58d46ed97185b18ab50c
saurav1998hub <58566328+saurav1998hub@users.noreply.github.com>
2024-04-03 00:02:27 +0530

    changes

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -6,7 +6,7 @@ import { AfterViewInit, Component, ElementRef, EventEmitter, Output, ViewChild }
   styleUrls: ['./timer.component.scss']
 })
 export class TimerComponent implements AfterViewInit {
-  targetDate: Date = new Date(2024, 3, 3);
+  targetDate: Date = new Date(2024, 3, 4);
   difference: number = 0;
   intervalId: number = 0;
   months: Array<string> = [
